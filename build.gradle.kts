8d55eba847ff92a6793e0ec26e02be8c7c06468b
Márton Braun <braunmarci@gmail.com>
2024-07-23 10:24:40 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.kotlinxSerialization) apply false
+    alias(libs.plugins.kmpNativeCoroutines) apply false
+    alias(libs.plugins.ksp) apply false
+}
